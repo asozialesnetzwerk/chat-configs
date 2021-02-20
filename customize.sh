c26bbc557746553673936de4783642340f08bb79
Joshix <joshix.dev@mailbox.org>
2021-02-20 02:12:21 +0100

    fix color

--- customize.sh
+++ customize.sh
@@ -15,4 +15,4 @@ sed -i 's/<link rel="shortcut icon" href="[^\n]*">/<link rel="shortcut icon" hre
 find webapp/ -type f -exec \
     sed -i 's/#0DBD8B/#C91616/g' {} +
 find webapp/ -type f -exec \
-    sed -i 's/#368BD6/#FFFFFF/g' {} +
+    sed -i 's/#368BD6/#000000/g' {} +
