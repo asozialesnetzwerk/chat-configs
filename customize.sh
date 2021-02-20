8e0194e2129d9a8ee657aa003cc9bc5683f1abf3
Joshix <joshix.dev@mailbox.org>
2021-02-20 14:35:42 +0100

    update

--- customize.sh
+++ customize.sh
@@ -3,7 +3,6 @@
 # Download images
 curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/kangaroo.svg > webapp/welcome/images/logo.svg
 curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/bio_brause.svg > webapp/welcome/images/matrix.svg
-curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/WebsiteIcon.svg > webapp/WebsiteIcon.svg
 curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/kangaroo_bg.png > webapp/themes/element/img/backgrounds/lake.jpg
 curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/WebsiteIcon.svg > webapp/themes/element/img/logos/element-logo.svg
 #replace Element with Asozialer Chat
@@ -11,7 +10,7 @@ find webapp/ -type f -exec \
     sed -i 's/Element/Asozialer Chat/g' {} +
 
 #replace icon
-sed -i 's/<link rel="shortcut icon" href="[^\n]*">/<link rel="shortcut icon" href="WebsiteIcon.svg">/g' webapp/index.html
+sed -i 's/<link rel="shortcut icon" href="[^\n]*">/<link rel="shortcut icon" href="themes\/element\/img\/logos/element-logo.svg">/g' webapp/index.html
 
 #improve colors:
 find webapp/ -type f -exec \
