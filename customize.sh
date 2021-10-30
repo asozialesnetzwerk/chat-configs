1df2d41d74dde31e85add63ac88fc63f41ac25c2
Joshix-1 <57299889+Joshix-1@users.noreply.github.com>
2021-10-30 15:21:54 +0000

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #replace Element with Asozialer Chat
-find webapp/ -type f \( -name "*.html" -or -name "*.json" \) -exec \
-    sed -i 's/Element/Asozialer Chat/g' {} +
+#find webapp/ -type f \( -name "*.html" -or -name "*.json" \) -exec \
+#    sed -i 's/Element/Asozialer Chat/g' {} +
 
 #improve colors:
 find webapp/ -type f \( -name "*.html" -or -name "*.svg" -or -name "*.css" \) -exec \
