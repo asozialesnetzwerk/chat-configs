2604e841f80ab80fb744626d61b4099c19432dd4
Joshix <joshix.dev@mailbox.org>
2022-01-16 14:40:22 +0100

    add png version of website icon

--- customize-element.sh
+++ customize-element.sh
@@ -5,6 +5,7 @@ curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/kanga
 curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/bio_brause.svg > webapp/welcome/images/matrix.svg
 curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/kangaroo_bg.png > webapp/themes/element/img/backgrounds/lake.jpg
 curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/WebsiteIcon.svg > webapp/themes/element/img/logos/element-logo.svg
+curl https://raw.githubusercontent.com/asozialesnetzwerk/chat-configs/main/website-icon-192.png > webapp/themes/element/img/logos/element-logo-192.png
 
 #replace icon
 sed -i 's|<link rel="shortcut icon" href="[^\n"]*">|<link rel="shortcut icon" href="themes/element/img/logos/element-logo.svg">|g' webapp/index.html
